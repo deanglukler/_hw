6bcc9def05d741af860bd2caaa0c1f3a8400a132
Dean Glueckler <glueckler@gmail.com>
2017-10-21 09:35:23 -0700

    _helloworld..

---
+++ zzzz/asdfc.js
@@ -0,0 +1,8 @@
+const da = Array(55).join('-');
+function cd() {console.log(da)}
+function c(i, d){if(d)console.log(da+'\n'+i+'\n'+da); else console.log(i);}
+function cc(desc, arg, d){
+  if(d) cd()
+  console.log(desc);console.log('typeof: ', typeof arg);console.dir(arg);
+  if(d) cd()
+}
---
+++ zzzz/asdfo.js
@@ -0,0 +1,5 @@
+let obj = {
+  p1: "property1",
+  p2: "property2",
+  p3: "property3"
+}
